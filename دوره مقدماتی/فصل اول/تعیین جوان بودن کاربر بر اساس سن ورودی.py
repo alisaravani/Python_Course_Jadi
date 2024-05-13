55d940b22908624e8ebb2f16e1e2f04201e9d95f
Ali Saravani <52594514+alisaravani@users.noreply.github.com>
2024-05-13 15:47:04 +0330

    Create تعیین جوان بودن کاربر بر اساس سن ورودی.py

---
+++ دوره مقدماتی/فصل اول/تعیین جوان بودن کاربر بر اساس سن ورودی.py
@@ -0,0 +1,14 @@
+Number = int(input())
+
+if Number>0 and Number<6:
+    print("khordsal")
+elif Number>=6 and Number<10:
+    print("koodak")
+elif Number>=10 and Number<14:
+    print("nojavan")
+elif Number>=14 and Number<24:
+    print("javan")
+elif Number>=24 and Number<40:
+    print("bozorgsal")
+elif Number>=40:
+    print("miansal")
