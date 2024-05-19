ea32a70ae333befc72449b84519783470b69ebbc
Ali Saravani <52594514+alisaravani@users.noreply.github.com>
2024-05-19 09:41:44 +0330

    Update قیمت لپ تاپ ها.py

--- دوره مقدماتی/فصل سوم/قیمت لپ تاپ ها.py
+++ دوره مقدماتی/فصل سوم/قیمت لپ تاپ ها.py
@@ -1,35 +1,18 @@
-strN=int(input())
-List1=[]
-ListPrice=[]
-ListQuality=[]
-strFind=0
-strTest=""
-Count=0
+strInput=""
+ListPrice,ListQuality=[],[]
+intIndex=0
 
-for i in range(strN):
-    List1.append(input())
-    strTest=str(List1[i])
-    strFind=strTest.find(" ")
-    ListPrice.append(int(strTest[:strFind]))
-    ListQuality.append(int(strTest[strFind:]))
+n=int(input())
+for i in range(0,n):
+    strInput=input()
+    ListPrice.append(int(strInput[0:strInput.index(" ")]))
+    ListQuality.append(int(strInput[strInput.index(" ")+1:]))
+for i in range(0,n):
+    for j in range(0,n):
+        if ListPrice[i]<=ListPrice[j] and ListQuality[i]>=ListQuality[j] and i!=j:
+            intIndex+=1
 
-min1=ListPrice.index(min(ListPrice))
-pop1=min(ListPrice)
-p1=ListQuality[min1]
-ListPrice.pop(min1)
-ListQuality.pop(min1)
-
-for i in range(strN):
-    if len(ListPrice)>0 or len(ListQuality)>0:
-        pop2=ListPrice.pop()
-        p2=ListQuality.pop()
-
-        if(pop1<pop2) and p1>p2:
-            Count=1
-            break
-        else:
-            Count=0
-if Count>0:
+if intIndex>=1:
     print("happy irsa")
 else:
     print("poor irsa")
